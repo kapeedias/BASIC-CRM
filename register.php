13e16e82bb08b51872dcafa462ca1fbba7665777
Sai Deepak <sai@livewd.ca>
2023-12-04 01:36:38 +0530

    D

--- register.php
+++ register.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     try {
         // Check if a user with the same email already exists
-        $stmt_check = $conn->prepare("SELECT user_id FROM users WHERE email = :email");
+        $stmt_check = $conn->prepare("SELECT user_id FROM crm_users WHERE email = :email");
         $stmt_check->bindParam(':email', $email);
         $stmt_check->execute();
 
@@ -40,9 +40,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $hashedPassword = hash('sha256', $password . $salt);
 
             // Prepare and execute the SQL query to insert user data into the database
-            $stmt = $conn->prepare("INSERT INTO users (username, password, salt, email) VALUES (:username, :password, :salt, :email)");
+            $stmt = $conn->prepare("INSERT INTO crm_users (username, password_hash, salt, email) VALUES (:username, :password_hash, :salt, :email)");
             $stmt->bindParam(':username', $username);
-            $stmt->bindParam(':password', $hashedPassword);
+            $stmt->bindParam(':password_hash', $hashedPassword);
             $stmt->bindParam(':salt', $salt);
             $stmt->bindParam(':email', $email);
             $stmt->execute();
